d2ec733f7cf6a152de46de955c348fb5b7a92bdb
kolipranali5555-hub <kolipranali5555@gmail.com>
2026-01-21 16:00:54 +0530

    List

---
+++ Examples.java
@@ -0,0 +1,145 @@
+package com.cframe;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Examples {
+	public static void main(String[] args) {
+//List
+		// ArrayList
+		ArrayList list = new ArrayList();
+//		list.add("Pranali");
+//		list.add(20);
+//		list.add(123);
+//		list.add("koli");
+//		list.add(true);
+//		list.add(null);
+//		list.add(657885648947l);
+//		list.add("p897mail.com");
+//		list.add("Sangli");
+
+		// System.out.println(list);
+
+//	    list.addFirst("Name");
+//      System.out.println(list);
+
+//	    list.remove(2);
+//	    System.out.println(list);
+
+//		list.clear();
+//		System.out.println(list);
+
+//		list.contains(false);
+//		System.out.println(list.contains("Miracles"));
+//list.getLast();
+//		System.out.println(list.getLast());
+//list.isEmpty();
+//		System.out.println(list.isEmpty());
+
+//		list.getFirst();
+//		System.out.println(list);
+
+		// Vector
+//		Vector v = new Vector();
+//		list.add(null);
+//		list.add("Arohi");
+//		list.add(74678857985l);
+//		list.add(false);
+//		list.add(67.90);
+//		list.add("pune");
+		// System.out.println(list);
+
+//		list.getFirst();
+//		System.out.println(list.getFirst());
+
+//		list.size();
+//		System.out.println(list.size());
+
+//		list.removeLast();
+//		System.out.println(list.removeLast());
+//		
+//		list.reversed();
+//		System.out.println(list.reversed());
+
+//		list.listIterator();
+//		System.out.println(list.listIterator());
+
+//		list.removeFirst();
+//		System.out.println(list.removeFirst());
+//		System.out.println(list);
+
+		// LinkedList
+//		LinkedList<String> Ll = new LinkedList<String>();
+//		Ll.add("Pranali");
+//		Ll.add("aarohi");
+//		Ll.add("sangli");
+//		Ll.add("delhi");
+//		Ll.add("pk7348gmail.com");
+//
+//		// System.out.println(Ll);
+//
+//		Ll.getFirst();
+//		System.out.println(Ll.getFirst());
+
+//		Ll.pollFirst();
+//		System.out.println(Ll.pollFirst());
+
+//		Ll.addLast("Ananya");
+//		System.out.println(Ll);
+
+//		Ll.peekFirst();
+//		System.out.println(Ll.peekFirst());
+
+//		Ll.offerFirst("Rohit");
+//		System.out.println(Ll);
+//		
+//		Ll.offerLast("Prathmesh");
+//		System.out.println(Ll);
+
+		// Generics
+//		ArrayList<String> list1 = new ArrayList<String>();
+//		list1.add("Pranali");
+//		list1.add("pkgmail.com");
+//		list1.add("mumbai");
+//		list1.add("Kashmir");
+//		list1.add("Ooty");
+//System.out.println(list);
+// list1.contains("ananya");
+//System.out.println(list.contains("ananya"));
+//		list1.get(0);
+//		System.out.println(list1.get(0));
+//		list1.set(0, "panu");
+//		System.out.println(list1.set(3, "panu"));
+
+		// System.out.println(list1);
+		
+// Vector
+//		Vector<String> v = new Vector<String>();
+//		v.add("Java");
+//		v.add("Python");
+//		v.add("Java");
+//		v.add("C++");
+//		v.add("c");
+//		v.add(null);
+//System.out.println(v.get(3));
+//		System.out.println(v);
+//		v.remove(0);
+	
+// LinkedList
+
+		LinkedList<Integer>Ll=new LinkedList<Integer>();
+		Ll.add(23);
+		Ll.add(89);
+		Ll.add(87);
+		Ll.add(89);
+		Ll.add(67);
+		
+		//System.out.println(Ll);
+		//System.out.println(Ll.get(2));
+//		Ll.getFirst();
+//		System.out.println(Ll.getFirst());
+		Ll.isEmpty();
+		System.out.println(Ll.isEmpty());
+	}
+}
